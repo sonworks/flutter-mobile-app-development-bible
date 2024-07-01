c01d489ae52274c1d5c14a5b0b30760606d4178c
seongwook.choi [sonworks] <seongwook.choi@goinc.jp>
2024-07-01 09:50:35 +0900

    3-1-3 StatefulWidget

--- chapter_3/flutter_application_1/lib/main.dart
+++ chapter_3/flutter_application_1/lib/main.dart
@@ -3,6 +3,8 @@ import 'package:flutter/material.dart'; // Flutterウィジェットが使用可
 // アプリが起動した時に、最初に動作する部分
 void main() => runApp(MyApp());
 
+/*
+// StatelessWidget Sample
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) => MaterialApp(
@@ -16,6 +18,53 @@ class MyApp extends StatelessWidget {
               'Flutter Demo Home Page',
               style: Theme.of(context).textTheme.displaySmall,
             ),
+          ),
+        ),
+      );
+}
+*/
+
+// StatefulWidget Sample
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+// State<...> ん中身にはWidgetのクラス名を書く
+class _MyAppState extends State<MyApp> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) => MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: Scaffold(
+          body: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Text(
+                  ' You have pushed the button this many times:',
+                ),
+                Text(
+                  '$_counter',
+                  style: Theme.of(context).textTheme.displaySmall,
+                ),
+              ],
+            ),
+          ),
+          floatingActionButton: FloatingActionButton(
+            onPressed: _incrementCounter,
+            tooltip: 'Increment',
+            child: Icon(Icons.add),
           ),
         ),
       );
