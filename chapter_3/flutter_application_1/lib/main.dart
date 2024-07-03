27f4b3af51e8545959814cc24410c1c492e1799e
seongwook.choi [sonworks] <seongwook.choi@goinc.jp>
2024-07-03 09:11:39 +0900

    3.2.1.4 Add Layout with Material Design

--- chapter_3/flutter_application_1/lib/main.dart
+++ chapter_3/flutter_application_1/lib/main.dart
@@ -24,47 +24,63 @@ class MyApp extends StatelessWidget {
 }
 */
 
-// StatefulWidget Sample
-class MyApp extends StatefulWidget {
-  @override
-  _MyAppState createState() => _MyAppState();
-}
+// // 3.1.3.5 StatefulWidget Sample
+// class MyApp extends StatefulWidget {
+//   @override
+//   _MyAppState createState() => _MyAppState();
+// }
 
-// State<...> ん中身にはWidgetのクラス名を書く
-class _MyAppState extends State<MyApp> {
-  int _counter = 0;
+// // State<...> ん中身にはWidgetのクラス名を書く
+// class _MyAppState extends State<MyApp> {
+//   int _counter = 0;
 
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
+//   void _incrementCounter() {
+//     setState(() {
+//       _counter++;
+//     });
+//   }
 
+//   @override
+//   Widget build(BuildContext context) => MaterialApp(
+//         title: 'Flutter Demo',
+//         theme: ThemeData(
+//           primarySwatch: Colors.blue,
+//         ),
+//         home: Scaffold(
+//           body: Center(
+//             child: Column(
+//               mainAxisAlignment: MainAxisAlignment.center,
+//               children: <Widget>[
+//                 Text(
+//                   ' You have pushed the button this many times:',
+//                 ),
+//                 Text(
+//                   '$_counter',
+//                   style: Theme.of(context).textTheme.displaySmall,
+//                 ),
+//               ],
+//             ),
+//           ),
+//           floatingActionButton: FloatingActionButton(
+//             onPressed: _incrementCounter,
+//             tooltip: 'Increment',
+//             child: Icon(Icons.add),
+//           ),
+//         ),
+//       );
+// }
+
+// 3.2.1.4 マテリアルデザインでレイアウトウィジェットをページに追加
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) => MaterialApp(
-        title: 'Flutter Demo',
-        theme: ThemeData(
-          primarySwatch: Colors.blue,
-        ),
+        title: 'Flutter layout demo',
         home: Scaffold(
-          body: Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                Text(
-                  ' You have pushed the button this many times:',
-                ),
-                Text(
-                  '$_counter',
-                  style: Theme.of(context).textTheme.displaySmall,
-                ),
-              ],
-            ),
+          appBar: AppBar(
+            title: Text('Flutter layout demo'),
           ),
-          floatingActionButton: FloatingActionButton(
-            onPressed: _incrementCounter,
-            tooltip: 'Increment',
-            child: Icon(Icons.add),
+          body: Center(
+            child: Text('Hello world'),
           ),
         ),
       );
