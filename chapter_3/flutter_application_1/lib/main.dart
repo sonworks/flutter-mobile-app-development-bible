0b1af25c1a82f4c0cb68cd510944bff4c9a437f4
seongwook.choi [sonworks] <seongwook.choi@goinc.jp>
2024-07-19 08:58:01 +0900

    3.2.2.2 Container

--- chapter_3/flutter_application_1/lib/main.dart
+++ chapter_3/flutter_application_1/lib/main.dart
@@ -222,3 +222,34 @@ class Pavolva extends StatelessWidget {
     );
   }
 }
+
+// 3.2.2.2 Container example
+Widget _buildImageContainer(List<String> images) {
+  final slicedImages = images.fold<List<List<String>>>([], (list, image) {
+    if (list.length == 0 || list.last.length >= 2) {
+      list.add(<String>[]);
+    }
+    list.last.add(image);
+    return list;
+  });
+  return Container(
+    decoration: BoxDecoration(
+      color: Colors.black26,
+    ),
+    child: Column(
+      children: slicedImages
+          .map((rowImages) => Row(
+              children: rowImages
+                  .map((image) => Container(
+                        decoration: BoxDecoration(
+                          border: Border.all(width: 10, color: Colors.black38),
+                          borderRadius: BorderRadius.all(Radius.circular(8)),
+                        ),
+                        margin: EdgeInsets.all(4),
+                        child: Image.asset(image, width: 150),
+                      ))
+                  .toList()))
+          .toList(),
+    ),
+  );
+}
