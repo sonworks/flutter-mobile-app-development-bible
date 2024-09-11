7a6cf7581db2f52bc339abb6faa33032c0859c2c
choi.seongwook sonworks <seongwook.choi@goinc.jp>
2024-09-11 09:28:18 +0900

    Remove comparison & Use 'required' keyword, not '@required' annotation

--- chapter_6/flutter_application_6/lib/models/event.dart
+++ chapter_6/flutter_application_6/lib/models/event.dart
@@ -4,12 +4,17 @@ import 'package:meta/meta.dart';
 class Event {
   final String id;
   final String title;
-  final String decription;
+  final String description;
   final DateTime date;
   final String imageUrl;
   //final User owner;
 
   Event({
-    @required this,id,
+    required this.id,
+    required this.title,
+    required this.description,
+    required this.date,
+    required this.imageUrl,
+    // @required this.owner,
   });
 }
