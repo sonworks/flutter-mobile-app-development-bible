a8b1ea6890c4b603437dc7cd2ab5931c416858c3
choi.seongwook sonworks <seongwook.choi@goinc.jp>
2024-09-25 09:22:33 +0900

    6.2.5.1 event_list_bloc.dart

---
+++ chapter_6/flutter_application_6/lib/bloc/event_list/event_list_bloc.dart
@@ -0,0 +1,30 @@
+import 'package:flutter_application_6/bloc/event_list/event_list_event.dart';
+import 'package:flutter_application_6/bloc/event_list/event_list_repository.dart';
+import 'package:flutter_application_6/bloc/event_list/event_list_state.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class EventListBloc extends Bloc<EventListEvent, EventListState> {
+  final EventListRepository _eventListRepository;
+
+  EventListBloc({required EventListRepository eventListRepository})
+      : _eventListRepository = eventListRepository;
+
+  @override
+  EventListState get initialState => EventListEmpty();
+
+  @override
+  Stream<EventListState> mapEventToState(EventListEvent event) async* {
+    if (event is EventListLoad) {
+      yield* _mapEventListLoadToState();
+    }
+  }
+
+  Stream<EventListState> _mapEventListLoadToState() async* {
+    yield EventListInProgress();
+    try {
+      yield EventListSuccess(eventList: _eventListRepository.fetch());
+    } catch (_) {
+      yield EventListFailure(error: _);
+    }
+  }
+}
